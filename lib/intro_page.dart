3115519a0c32a4a2e39b4ce60c20331723670cde
Karan Shah <karanshah0206@outlook.com>
2025-04-05 19:36:51 +0530

    Intro page styling udpates

--- lib/intro_page.dart
+++ lib/intro_page.dart
@@ -9,19 +9,25 @@ class IntroPage extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset("assets/images/quiz-logo.png", width: 300),
+          Image.asset(
+            "assets/images/quiz-logo.png",
+            width: 300,
+            color: Color.fromARGB(175, 255, 255, 255),
+          ),
           const SizedBox(height: 25),
           const Text(
             "Learn Flutter the fun way!",
-            style: TextStyle(color: Colors.white, fontSize: 22),
-          ),
-          const SizedBox(height: 35),
-          OutlinedButton(
-            child: const Text(
-              "Start Quiz",
-              style: TextStyle(color: Colors.white, fontSize: 20),
+            style: TextStyle(
+              color: Color.fromARGB(195, 255, 255, 255),
+              fontSize: 22,
             ),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
             onPressed: () => {},
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            label: const Text("Start Quiz"),
+            icon: const Icon(Icons.arrow_forward),
           ),
         ],
       ),
