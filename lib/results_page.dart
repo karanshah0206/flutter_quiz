659eeff209c2de7420dd594e4a6adddeee2864ce
Karan Shah <karanshah0206@outlook.com>
2025-04-06 03:10:57 +0530

    Results page logic and frontend completed

--- lib/results_page.dart
+++ lib/results_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quiz/data/questions.dart';
 
 class ResultsPage extends StatelessWidget {
   final List<String> selectedAnswers;
@@ -6,19 +7,110 @@ class ResultsPage extends StatelessWidget {
 
   const ResultsPage(this.selectedAnswers, this.nextPage, {super.key});
 
+  List<Map<String, Object>> getResults() {
+    final List<Map<String, Object>> summary = [];
+    for (int i = 0; i < selectedAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].questionText,
+        'selected_answer': selectedAnswers[i],
+        'correct_answer':
+            questions[i].answerOptions[questions[i].correctAnswerIndex],
+      });
+    }
+    return summary;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
+    final List<Map<String, Object>> resultsData = getResults();
+    final int questionsCount = questions.length;
+    final int correctAnswersCount =
+        resultsData.where((result) {
+          return result['selected_answer'] == result['correct_answer'];
+        }).length;
+
+    return Container(
+      padding: EdgeInsets.all(40),
       child: Column(
-        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(
-            'Your results',
-            style: TextStyle(color: Colors.white, fontSize: 24),
+            'Correctly answered $correctAnswersCount out of $questionsCount',
+            style: TextStyle(
+              color: Color.fromARGB(200, 255, 255, 255),
+              fontSize: 20,
+            ),
           ),
-          OutlinedButton.icon(
+          SizedBox(height: 20),
+          SizedBox(
+            height: 330,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  ...resultsData.map((summary) {
+                    return Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        CircleAvatar(
+                          maxRadius: 14,
+                          backgroundColor:
+                              summary['selected_answer'] ==
+                                      summary['correct_answer']
+                                  ? const Color.fromARGB(255, 104, 242, 228)
+                                  : const Color.fromARGB(255, 201, 115, 214),
+                          child: Text(
+                            ((summary['question_index'] as int) + 1).toString(),
+                          ),
+                        ),
+                        SizedBox(width: 15),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                summary['question'].toString(),
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              SizedBox(height: 5),
+                              Text(
+                                summary['selected_answer'] as String,
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: const Color.fromARGB(
+                                    255,
+                                    201,
+                                    115,
+                                    214,
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                summary['correct_answer'] as String,
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: const Color.fromARGB(
+                                    255,
+                                    104,
+                                    242,
+                                    228,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    );
+                  }),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 30),
+          TextButton.icon(
             onPressed: nextPage,
-            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            style: TextButton.styleFrom(foregroundColor: Colors.white),
             label: const Text('Return Home'),
             icon: const Icon(Icons.replay),
           ),
