273bce93c76f3042b644cebebd3fcc3a8a7879c1
Karan Shah <karanshah0206@outlook.com>
2025-04-06 00:34:21 +0530

    Design question page view with dynamic answer options loading

--- lib/questions_page.dart
+++ lib/questions_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_quiz/data/questions.dart';
+
 class QuestionsPage extends StatefulWidget {
   const QuestionsPage({super.key});
 
@@ -10,8 +12,37 @@ class QuestionsPage extends StatefulWidget {
 }
 
 class _QuestionsState extends State<QuestionsPage> {
+  int _questionIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Text('');
+    return Container(
+      padding: EdgeInsets.all(20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            questions[_questionIndex].questionText,
+            style: TextStyle(color: Colors.white),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 30),
+          ...questions[_questionIndex].answerOptions.map(
+            (answerOption) => ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 33, 1, 95),
+                foregroundColor: const Color.fromARGB(175, 255, 255, 255),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+              child: Text(answerOption),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
